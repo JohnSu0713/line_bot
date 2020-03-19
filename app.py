c7ffcb1b7e6d1b26b6879a461fccc8c4a3121eb6
John Su <johnsu0713@gmail.com>
2020-03-19 18:54:14 +0800

    add 招呼 msg

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ def handle_message(event):
         r = "這個世界上，只有『吃飽沒？』值得問泥泥...泥泥胖！！"
     elif "吃飽沒？" in msg:
         r = "您說笑嗎？泥泥鄙視你"
+    elif msg in ["你好","泥泥你好", "嗨泥泥"]
+        r = "泥泥沒空，泥泥在吃東西，吃東西皇帝大！！"
 
 
     line_bot_api.reply_message(

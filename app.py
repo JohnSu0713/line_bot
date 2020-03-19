d561c6f9340da6bb83f356d2e7b6169bcad84a6d
John Su <johnsu0713@gmail.com>
2020-03-19 18:15:59 +0800

    add food msg

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-import random
-
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('2MMEZj3CbTCP6rt6UYYueBD9MsNnIjFAksct60sVTurfG2zg/Q/3oj9rTDXoldcYw1kpQr1Rit4qaU/qC4z0DUP1ufmUquHJQzDxriissAtciyL1U/rBzEzx5JPHIcbRvuzImGpARrsmLFu+rSFnsQdB04t89/1O/w1cDnyilFU=')
@@ -40,25 +38,15 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    reply = "公啥毀？泥泥還小，聽不懂啦！～"
+    r = "供殺毀？泥泥還小，聽不懂啦！～"
 
-    if msg in ["食物吃飼料罐罐貓食物吃飽肚子餓"]
-        reply = "泥泥永遠餓著呢..."
-        
+    if msg in "食物罐罐頭貓飼料吃飽餵食":
+        r = "泥泥永遠餓著呢...泥泥胖!!"
+
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=reply))
-
-@app.route("/StickerSendMessage/")
-def location_send_message():
-    reply_s = random.int(1, 430)
-    sticker_message = StickerSendMessage(
-        package_id = '1',
-        sticker_id = reply_s
-    )
-    line_bot_api.push_message(user_id, sticker_message)
-    return 'StickerSendMessage Done!'
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":

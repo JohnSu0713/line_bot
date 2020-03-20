a412acac8cf3919bbabd1913aa8e133847798b71
John Su <johnsu0713@gmail.com>
2020-03-20 13:16:59 +0800

    add_stickers

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+	MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
+
+import random
 
 app = Flask(__name__)
 
@@ -18,48 +20,57 @@ handler = WebhookHandler('1989623446333031b8adaceca81bd94d')
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		print("Invalid signature. Please check your channel access token/channel secret.")
+		abort(400)
 
-    return 'OK'
+	return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    r = "泥泥笨，聽不懂..."
+	msg = event.message.text
+	r = "泥泥笨，聽不懂..."
 
-    if msg in "食物罐罐頭貓飼料吃飽餵食了沒嗎？":
-        r = "泥泥永遠餓著呢...泥泥胖!!"
-    elif "嗨" in msg:
-        r = "泥泥餓，沒空跟你說『嗨』啦..."
-    elif "泥爸" in msg:
-        r = "泥爸是個超級大帥哥-A.K.A 『永安金城武』是他！！"
-    elif "泥媽" in msg:
-        r = "泥媽跟泥泥一樣，好愛好愛吃東西哦"
-    elif "嗎" in msg:
-        r = "本貓只想著吃，你怎麼問題這麼多？"
+	if "貼圖" in msg:
+		message = StickerSendMessage(
+		package_id='1',
+		sticker_id=str(random.int(1, 18)))
+		line_bot_api.reply_message(event.reply_token, message)
 
 
+	if msg in "食物罐罐頭貓飼料吃飽餵食了沒嗎？":
+		r = "泥泥永遠餓著呢...泥泥胖!!"
+	elif "嗨" in msg:
+		r = "泥泥餓，沒空跟你說『嗨』啦..."
+	elif "泥爸" in msg:
+		r = "泥爸是個超級大帥哥-A.K.A 『永安金城武』是他！！"
+	elif "泥媽" in msg:
+		r = "泥媽跟泥泥一樣，好愛好愛吃東西哦"
+	elif "嗎" in msg:
+		r = "本貓只想著吃，你怎麼問題這麼多？"
+	elif "呢" in msg:
+		r = "本貓只想著吃，你怎麼問題這麼多？"
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=r))
+
+
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
 
 
 
